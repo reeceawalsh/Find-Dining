0af77e6752a3124cc415878afa723c6f0fccce34
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-19 20:41:12 +0000

    Setup frontend and backend. Also added a Login landing page with some styling to give a little example of how things are going to look

---
+++ frontend/components/Login.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image"
+import Link from 'next/link'
+
+// variables
+const wrongDetails = true
+
+export default function Login() {
+  
+  const login = () => {
+    // send request to login
+  }
+
+  return (
+    <div className="container Login__Container blue-background black-text">
+      <Link className="Login__Skip" href="/Home">Skip</Link> 
+      <h1 className="title">Find Dining</h1>
+      <img 
+        src="/LogoCropped.png" 
+        className="app-logo" 
+        height="170" 
+        width="240" 
+        alt="logo" 
+        />
+      <div className="Login__Login-form">
+        <form onSubmit={login}>
+          <label>Email</label>
+          <input 
+            name="email"
+            type="text"
+            placeholder="Email Address"
+          />
+          <label>Password</label>
+          <input 
+            name="password"
+            type="text"
+            placeholder="Password"
+          />
+          <div className="Login__Error-message">
+            {wrongDetails && <p>Incorrect login details.</p>}
+          </div>
+          <div className="Login__Buttons">
+            <button type="submit">Login</button>
+            <button type="submit">Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
---
+++ frontend/components/ThirdPartyLogin.jsx
@@ -0,0 +1,14 @@
+export default function ThirdPartyLogin() {
+  return (
+    <div className="container ThirdPartyLogin__Container peach-background">
+      <h2>Continue with...</h2>
+      <div className="ThirdPartyLogin__Icons">
+        <h3>Facebook Icon</h3>
+        <h3>Google Icon</h3>
+        <h3>Instagram Icon</h3>
+        <h3>Apple Icon</h3>
+      </div>
+    </div>
+  )
+}
+
