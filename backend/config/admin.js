042cf9b7ee249cda83399b0c6ed148f627164757
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-25 12:33:35 +0000

    Everything is fully setup

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT", "2'&Pz:`et{eEnLdp'bdW.&F"),
+  },
+  auth: {
+    secret: env("ADMIN_JWT_SECRET", "3b0c993312109e6794989ee2c3c35c78"),
   },
 });
--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL'),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "finddiningdata"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "password"),
+      ssl: env.bool("DATABASE_SSL", true),
     },
   },
 });
