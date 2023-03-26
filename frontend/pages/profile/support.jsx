20c8fb4c660836682d902207da0e92dadaf21aa3
shaktiv8 <99215550+shaktiv8@users.noreply.github.com>
2023-03-27 03:39:42 +0800

    support page

--- frontend/pages/profile/support.jsx
+++ frontend/pages/profile/support.jsx
@@ -1,8 +1,20 @@
 // Support Page
 export default function Support() {
     return (
-        <div className="container">
-            <h1>Support Page</h1>
+      <div className="container">
+        <h1>Support Page</h1>
+        <div className="support-question">
+          <h2>How do I find a restaurant near me?</h2>
+          <p>To find a restaurant near you, simply give our app location access, or enter your location in the search bar on the homepage and click "Search".</p>
         </div>
+        <div className="support-question">
+          <h2>How do I add a new restaurant to the favourites list?</h2>
+          <p>To add a new restaurant to you favourites list, go to the restaurant's page and click on the hear shaped button</p>
+        </div>
+        <div className="support-question">
+          <h2>How do I view my favourite restaurants</h2>
+          <p>To view favourite restaurants, click on the favourites button on the homepage to display a list of your saved restaurants</p>
+        </div>
+      </div>
     );
-}
+    }
