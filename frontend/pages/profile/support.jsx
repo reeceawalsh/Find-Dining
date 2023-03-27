20d9ff33f341bb0eb4cdafe9666a5c2174325fc0
shaktiv8 <99215550+shaktiv8@users.noreply.github.com>
2023-03-28 01:19:09 +0800

    support page updated

--- frontend/pages/profile/support.jsx
+++ frontend/pages/profile/support.jsx
@@ -1,20 +1,33 @@
 // Support Page
-export default function Support() {
+function SupportLink(props) {
+    return (
+      <div className="support-link">
+        <h3>{props.title}</h3>
+        <p>{props.body}</p>
+      </div>
+    );
+  }
+  
+  export default function Support() {
     return (
       <div className="container">
         <h1>Support Page</h1>
-        <div className="support-question">
-          <h2>How do I find a restaurant near me?</h2>
-          <p>To find a restaurant near you, simply give our app location access, or enter your location in the search bar on the homepage and click "Search".</p>
-        </div>
-        <div className="support-question">
-          <h2>How do I add a new restaurant to the favourites list?</h2>
-          <p>To add a new restaurant to you favourites list, go to the restaurant's page and click on the hear shaped button</p>
-        </div>
-        <div className="support-question">
-          <h2>How do I view my favourite restaurants</h2>
-          <p>To view favourite restaurants, click on the favourites button on the homepage to display a list of your saved restaurants</p>
-        </div>
+        <SupportLink
+          title="How do I make a reservation?"
+          body="To make a reservation, please visit our website or call our restaurant directly."
+        />
+        <SupportLink
+          title="What are your hours of operation?"
+          body="Our restaurant is open from 11am to 9pm every day of the week."
+        />
+        <SupportLink
+          title="Do you offer vegetarian options?"
+          body="Yes, we have several vegetarian options on our menu."
+        />
+        <SupportLink
+          title="How do I provide feedback on my experience?"
+          body="We value your feedback and would love to hear about your experience. Please email us at feedback@newcastlerestaurants.com."
+        />
       </div>
     );
-    }
+  }
