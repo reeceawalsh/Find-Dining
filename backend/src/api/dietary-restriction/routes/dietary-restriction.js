913a865aa6597a8ba5fc2faf188c2c54d99919f5
Tharan Patel <tharanpatel@Tharans-Air.broadband>
2023-03-28 18:27:57 +0100

    push to update strapi collections

---
+++ backend/src/api/dietary-restriction/routes/dietary-restriction.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dietary-restriction router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::dietary-restriction.dietary-restriction');
