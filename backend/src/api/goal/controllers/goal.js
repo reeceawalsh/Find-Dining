10ef4194d4e6bb33b36bc572fdce88a0aec41441
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-28 20:02:53 +0100

    Added goals to strapi

---
+++ backend/src/api/goal/controllers/goal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * goal controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::goal.goal');
---
+++ backend/src/api/goal/routes/goal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * goal router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::goal.goal');
