9002151c5344898445089422adc85829b0bfd30d
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-29 10:24:51 +0100

    Changed schemas to remove uuids

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,14 +1,23 @@
 module.exports = ({ env }) => ({
-    'transformer': {
-       enabled: true,
-       config: {
-         responseTransforms: {
-           removeAttributesKey: true,
-           removeDataKey: true
-         }
-       }
-     },
-     'import-export-entries': {
-       enabled: true
-     }
-   })
+  transformer: {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    },
+  },
+  "import-export-entries": {
+    enabled: true,
+  },
+  // email: {
+  //   config: {
+  //     provider: "sendmail",
+  //     settings: {
+  //       defaultFrom: "find-dining-no-reply@hotmail.com",
+  //       defaultReplyTo: "find-dining-no-reply@hotmail.com",
+  //     },
+  //   },
+  // },
+});
