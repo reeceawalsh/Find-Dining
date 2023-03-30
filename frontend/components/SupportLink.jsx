f56396cdfad0a2fe8d91163d49341f049827d231
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-03-30 06:33:42 +0100

    Added support page that Shakti has started work on and deleted some unneccessary files

---
+++ frontend/components/SupportLink.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import styles from "../pages/profile/styles/support.module.css";
+
+const SupportLink = ({ title, link, description }) => {
+    return (
+        <div className={styles.linkContainer}>
+            <a href={link} className={styles.link}>
+                <h3 className={styles.title}>{title}</h3>
+                <p className={styles.description}>{description}</p>
+            </a>
+        </div>
+    );
+};
+
+export default SupportLink;
--- frontend/pages/profile/support.jsx
+++ frontend/pages/profile/support.jsx
@@ -1,8 +1,35 @@
-// Support Page
+import React from "react";
+import Header from "../../components/Header";
+import SupportLink from "../../components/SupportLink";
+
+import styles from "./styles/support.module.css";
+
 export default function Support() {
     return (
-        <div className="container">
-            <h1>Support Page</h1>
+        <div className={styles.container}>
+            <Header name="Support" backButton />
+            <div className={styles.linksContainer}>
+                <SupportLink
+                    title="Contact Us"
+                    link="/contact page not done yet"
+                    description="Need help with something specific? Reach out to us directly."
+                />
+                <SupportLink
+                    title="App Feedback"
+                    link="www google survery not done yet "
+                    description="Let us know how we can improve your experience with our app."
+                />
+                <SupportLink
+                    title="Privacy Policy"
+                    link="/privacy-policy.pdf not done yet, on github "
+                    description="Read about our commitment to your privacy."
+                />
+                <SupportLink
+                    title="Terms of Use"
+                    link="/terms-of-use.pdf not done yet too, on github"
+                    description="Understand the terms and conditions of using our app."
+                />
+            </div>
         </div>
     );
 }
