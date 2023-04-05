1c2288f94f9f46edbdeec24399e9c40503116baf
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-04-05 13:58:04 +0100

    Updated dietary restriction css

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export default function Header(props) {
     return (
         <div className="container">
-            <h1>{props && props.name}</h1>
-            <h2>{props.save && props.save}</h2>
+            <h1>{props.name && props.name}</h1>
         </div>
     );
 }
