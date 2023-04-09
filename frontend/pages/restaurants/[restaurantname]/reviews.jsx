59177fa780e0a97b3de0f6258deb47c312ace07f
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-04-09 11:49:57 +0100

    Added hook for getting custom restaurant name and formatting it

--- frontend/pages/restaurants/[restaurantname]/reviews.jsx
+++ frontend/pages/restaurants/[restaurantname]/reviews.jsx
@@ -1,13 +1,16 @@
 import { useUser } from "@component/lib/authContext";
 import Layout from "@component/components/Layout";
+import Header from "@component/components/Header";
+import useRestaurantName from "../../../lib/useRestaurantName";
 
 export default function Reviews() {
     const { user, loading } = useUser();
+    const restaurantName = useRestaurantName();
 
     return (
         <Layout user={user}>
             <div className="container">
-                <h1>Specific Restaurant Reviews</h1>
+                <Header name={`${restaurantName} - Reviews`} />
             </div>
         </Layout>
     );
