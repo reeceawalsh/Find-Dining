2cc5dae288bb599183d66ef69ba24e2cf8959450
FelixLiu666 <c0017299@newcastle.ac.uk>
2023-04-11 18:21:42 +0100

    Add dialogues

--- frontend/pages/profile/accountdetails.jsx
+++ frontend/pages/profile/accountdetails.jsx
@@ -1,3 +1,12 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import Header from "@component/components/Header";
 import HeaderWithSave from "@component/components/HeaderWithSave";
 import { useFormControl } from '@mui/material/FormControl';
@@ -8,6 +17,18 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 
 // Account Details Page
 export default function AccountDetails() {
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <div className="container">
             <HeaderWithSave name="Account Details Page" />
@@ -27,8 +48,60 @@ export default function AccountDetails() {
             </div>
             <div className="info-section-container">
                 <div>Find Dining will never post your account without your express permission.</div>
-                <div>Delete all saved data.</div>
-                <div>Delete account</div>
+                <br></br>
+                <div>
+                    <Button variant="outlined" onClick={handleClickOpen}>
+                        Delete all saved data
+                    </Button>
+                    <Dialog
+                        fullScreen={fullScreen}
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="responsive-dialog-title"
+                    >
+                        <DialogContent>
+                            <DialogContentText>
+                                Are you sure you want to delete all your saved data?
+                                <br></br>(This will include your history and achievements)
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button autoFocus onClick={handleClose}>
+                                Yes
+                            </Button>
+                            <Button onClick={handleClose} autoFocus>
+                                No
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
+                <br></br>
+                <div>
+                <Button variant="outlined" onClick={handleClickOpen}>
+                        Delete account
+                    </Button>
+                    <Dialog
+                        fullScreen={fullScreen}
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="responsive-dialog-title"
+                    >
+                        <DialogContent>
+                            <DialogContentText>
+                                Are you sure you want to delete all your saved data?
+                                <br></br>(This will include your history and achievements)
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button autoFocus onClick={handleClose}>
+                                Yes
+                            </Button>
+                            <Button onClick={handleClose} autoFocus>
+                                No
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
             </div>
         </div>
     );
