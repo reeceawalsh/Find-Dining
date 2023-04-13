d76f60af1fd7ac0fc46f2db421590a731c93f3ad
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-04-13 13:15:25 +0100

    fixed another bug

--- frontend/pages/profile/accountdetails.jsx
+++ frontend/pages/profile/accountdetails.jsx
@@ -1,17 +1,16 @@
-
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { useTheme } from '@mui/material/styles';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 import Header from "@component/components/Header";
 import HeaderWithSave from "@component/components/HeaderWithSave";
-import { useFormControl } from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
+import { useFormControl } from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
 import GoogleIcon from "@mui/icons-material/Google";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -22,7 +21,7 @@ export default function AccountDetails() {
     const [openAccount, setOpenAccount] = React.useState(false);
     const theme = useTheme();
 
-    const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
 
     const handleClickOpenSavedData = () => {
         setOpenSavedData(true);
@@ -44,26 +43,69 @@ export default function AccountDetails() {
         <div className="container">
             <HeaderWithSave name="Account Details Page" />
             <div className="info-section-container">
-                <div className="info-section-item"><TextField fullWidth id="filled-basic" label="Name" variant="filled" /></div>
-                <div className="info-section-item"><TextField fullWidth id="filled-basic" label="City" variant="filled" /></div>
-                <div className="info-section-item"><TextField fullWidth id="filled-basic" label="Email" variant="filled" /></div>
-                <div className="info-section-item"><TextField fullWidth id="filled-basic" label="Password" variant="filled" /></div>
-            </div>
-            <div className="info-section-container">
                 <div className="info-section-item">
-                    <h2 style={{ paddingLeft: '1rem' }}>Social Accounts</h2>
+                    <TextField
+                        fullWidth
+                        id="filled-basic"
+                        label="Name"
+                        variant="filled"
+                    />
+                </div>
+                <div className="info-section-item">
+                    <TextField
+                        fullWidth
+                        id="filled-basic"
+                        label="City"
+                        variant="filled"
+                    />
+                </div>
+                <div className="info-section-item">
+                    <TextField
+                        fullWidth
+                        id="filled-basic"
+                        label="Email"
+                        variant="filled"
+                    />
+                </div>
+                <div className="info-section-item">
+                    <TextField
+                        fullWidth
+                        id="filled-basic"
+                        label="Password"
+                        variant="filled"
+                    />
                 </div>
             </div>
             <div className="info-section-container">
-                <div><FacebookIcon className="social-media-icon" />Facebook</div>
-                <div><InstagramIcon className="social-media-icon" />Instagram</div>
-                <div><GoogleIcon className="social-media-icon" />Google</div>
+                <div className="info-section-item">
+                    <h2 style={{ paddingLeft: "1rem" }}>Social Accounts</h2>
+                </div>
             </div>
             <div className="info-section-container">
-                <div>Find Dining will never post your account without your express permission.</div>
+                <div>
+                    <FacebookIcon className="social-media-icon" />
+                    Facebook
+                </div>
+                <div>
+                    <InstagramIcon className="social-media-icon" />
+                    Instagram
+                </div>
+                <div>
+                    <GoogleIcon className="social-media-icon" />
+                    Google
+                </div>
+            </div>
+            <div className="info-section-container">
+                <div>
+                    Find Dining will never post your account without your
+                    express permission.
+                </div>
                 <br></br>
                 <div>
-                    <Button variant="outlined" onClick={handleClickOpenSavedData}>
+                    <Button
+                        variant="outlined"
+                        onClick={handleClickOpenSavedData}
+                    >
                         Delete all saved data
                     </Button>
                     <Dialog
@@ -74,8 +116,10 @@ export default function AccountDetails() {
                     >
                         <DialogContent>
                             <DialogContentText>
-                                Are you sure you want to delete all your saved data?
-                                <br></br>(This will include your history and achievements)
+                                Are you sure you want to delete all your saved
+                                data?
+                                <br></br>(This will include your history and
+                                achievements)
                             </DialogContentText>
                         </DialogContent>
                         <DialogActions>
@@ -90,7 +134,7 @@ export default function AccountDetails() {
                 </div>
                 <br></br>
                 <div>
-                <Button variant="outlined" onClick={handleClickOpenAccount}>
+                    <Button variant="outlined" onClick={handleClickOpenAccount}>
                         Delete account
                     </Button>
                     <Dialog
@@ -117,4 +161,5 @@ export default function AccountDetails() {
                 </div>
             </div>
         </div>
-
+    );
+}
