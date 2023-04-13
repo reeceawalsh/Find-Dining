1f0ca3e83339c2f8d1062d32be9b394b3dc2026f
shaktiv8 <99215550+shaktiv8@users.noreply.github.com>
2023-04-13 23:18:50 +0800

    these are the tests for form elements
    I think it may also include the json flies we were doing before reece

---
+++ frontend/__tests__/formElements/DateInput.test.jsx
@@ -0,0 +1,28 @@
+import DateInput from "@component/components/FormElements/DateInput";
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("<DateInput />", () => {
+    test("On initial render value should be empty.", () => {
+        render(<DateInput name="birthdate" placeholder="Birth Date" />);
+
+        // check that the value is initially empty
+        expect(screen.getByPlaceholderText(/birth date/i).value).toEqual("");
+    });
+
+    test("User should be able to select a date", () => {
+        render(<DateInput name="birthdate" placeholder="Birth Date" />);
+
+        // check that the value is initially empty
+        expect(screen.getByPlaceholderText(/birth date/i).value).toEqual("");
+
+        // change the value to 2023-04-09
+        fireEvent.change(screen.getByPlaceholderText(/birth date/i), {
+            target: { value: "2023-04-09" },
+        });
+
+        // check that the value is 2023-04-09
+        expect(screen.getByPlaceholderText(/birth date/i).value).toEqual(
+            "2023-04-09"
+        );
+    });
+});
---
+++ frontend/__tests__/formElements/PasswordInput.test.jsx
@@ -0,0 +1,38 @@
+import PasswordInput from '@component/components/FormElements/PasswordInput';
+import { render, screen, fireEvent } from "@testing-library/react";
+
+describe("<PasswordInput />", () => {
+  test("User should be able to toggle password visibility", () => {
+    const mockOnChange = jest.fn();
+    render(
+      <PasswordInput
+        name="password"
+        placeholder="Password"
+        value=""
+        onChange={mockOnChange}
+        error={false}
+      />
+    );
+
+    // Find the password input
+    const passwordInput = screen.getByPlaceholderText("Password");
+
+    // Check that the initial input type is "password" (meaning password is hidden)
+    expect(passwordInput.type).toBe("password");
+
+    // Find the eye icon button
+    const eyeIconButton = screen.getByRole("button");
+
+    // Toggle visibility
+    fireEvent.click(eyeIconButton);
+
+    // Check that the input type changes to "text" (meaning password is visible)
+    expect(passwordInput.type).toBe("text");
+
+    // Toggle visibility back
+    fireEvent.click(eyeIconButton);
+
+    // Check that the input type changes back to "password" (meaning password is hidden)
+    expect(passwordInput.type).toBe("password");
+  });
+});
