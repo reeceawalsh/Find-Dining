cabf948c48908fe3d3c15c798de878ce5283540b
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-04-19 09:19:08 +0100

    Added conditionals to ensureinformation only shows up if it's available

--- frontend/components/Restaurant.jsx
+++ frontend/components/Restaurant.jsx
@@ -2,20 +2,32 @@ import styles from "./styles/restaurant.module.css";
 const Restaurant = ({ restaurant }) => {
     return (
         <div className={styles.restaurantWrapper}>
-            <h2>{restaurant.name}</h2>
-            <img
+            <h2 className={styles.restaurantName}>{restaurant.name}</h2>
+            {/* <img
                 src={restaurant.image_url}
                 alt={restaurant.name}
                 width="240"
                 height="240"
-            />
+            /> */}
             <div className={styles.restaurantInformation}>
-                <p>Rating: {restaurant.rating}</p>
-                <p>Review Count: {restaurant.review_count}</p>
-                <p>Distance: {Math.round(restaurant.distance)} meters</p>
-                <p>Address: {restaurant.location.address1}</p>
-                <p>Phone: {restaurant.display_phone}</p>
-                <p>Price: {restaurant.price}</p>
+                {restaurant.rating && <p>Rating: {restaurant.rating}</p>}
+                {/* {restaurant.review_count && (
+                    <p>Review Count: {restaurant.review_count}</p>
+                )} */}
+                {restaurant.distance && (
+                    <p>
+                        Distance away: {Math.round(restaurant.distance)} meters
+                    </p>
+                )}
+                {restaurant.location.address1 && (
+                    <p>Address: {restaurant.location.address1}</p>
+                )}
+                {restaurant.display_phone && (
+                    <p>Phone: {restaurant.display_phone}</p>
+                )}
+                {restaurant.price && <p>Price: {restaurant.price}</p>}
+                <p>Favourite Icon</p>
+                <p>Visited Icon</p>
             </div>
         </div>
     );
