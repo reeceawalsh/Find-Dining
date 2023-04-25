d9331e6f5f98109ab5abc7148d8ba09927a91d13
YaAn Ren <renliu2016@sina.com>
2023-04-25 23:06:08 +0800

    add alertDialog component (used for all kinds of popup)

---
+++ frontend/components/AlertDialog.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function AlertDialog({ openAlertDialogText, alertDialogContent, handleClickYes, handleClickNo }) {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleCloseYes = () => {
+        setOpen(false);
+        handleClickYes();
+    };
+
+    const handleCloseNo = () => {
+        setOpen(false);
+        handleClickNo();
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                {openAlertDialogText}
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {alertDialogContent.title}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {alertDialogContent.text}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseNo}>No</Button>
+                    <Button onClick={handleCloseYes} autoFocus>
+                        Yes
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
