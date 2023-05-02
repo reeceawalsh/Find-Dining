a3fc18d6209ad411cd6456c15977a662e085927d
reeceawalsh <reeceawalsh@hotmail.co.uk>
2023-05-02 15:27:45 +0100

    Map not shares restaurant list with RestaurantsList

--- frontend/components/RestaurantsList.jsx
+++ frontend/components/RestaurantsList.jsx
@@ -1,96 +1,20 @@
-import { useState, useEffect, useRef } from "react";
-import axios from "axios";
-import useLocation from "@component/lib/useLocation";
+import { useState, useEffect, useRef, useMemo } from "react";
 import Restaurant from "./Restaurant";
 import styles from "./styles/restaurantsList.module.css";
-import cuisines from "../lib/cuisines.json";
-import filters from "../lib/filters.json";
 import { useUser } from "@component/lib/authContext";
 import fetchUserData from "@component/lib/fetchUserData";
-import fetchRestaurantID from "@component/lib/fetchRestaurantID";
 
-const RestaurantsList = () => {
-    const { user, loading } = useUser();
-    const location = useLocation();
-    const loader = useRef(null);
-    const { lat, lng } = location;
-    const [restaurants, setRestaurants] = useState([]);
-    const [cuisine, setCuisine] = useState("");
-    const [renderedIds, setRenderedIds] = useState([]);
+const RestaurantsList = ({
+    setPage,
+    restaurants,
+    noMoreRestaurants,
+    loader,
+}) => {
+    const { user } = useUser();
     const [favourites, setFavourites] = useState([]);
     const [userData, setUserData] = useState(null);
-
-    // can sort by rating, review_count, distance, price, open_now and best_match (Default)
-    const [sortType, setSortType] = useState("best_match");
-
-    // Yelps maximum radius is 40k
-    const [radius, setRadius] = useState(40000);
-    const [page, setPage] = useState(1);
-
-    // fetch data from yelp for the restaurants
-    const fetchData = async () => {
-        try {
-            const response = await axios.get("/api/yelp", {
-                params: {
-                    latitude: lat,
-                    longitude: lng,
-                    term: cuisine,
-                    sort_by: sortType,
-                    radius: radius,
-                    offset: (page - 1) * 10,
-                    limit: 10,
-                },
-            });
-
-            // will only add restaurants that haven't already been added as the yelp api gives back duplicate results sometimes.
-            const newRestaurants = response.data.businesses.filter(
-                (restaurant) => {
-                    return !renderedIds.includes(restaurant.id);
-                }
-            );
-            const newIds = newRestaurants.map((restaurant) => restaurant.id);
-            renderedIds.push(...newIds);
-            setRestaurants((prevRestaurants) => {
-                return [...prevRestaurants, ...newRestaurants];
-            });
-            newRestaurants.forEach(async (restaurant) => {
-                await fetchRestaurantID(restaurant.id, restaurant.name);
-            });
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, [cuisine, sortType, lat, lng, page]);
-
-    // handles the cuisine dropdown option changing
-    const handleCuisineChange = (e) => {
-        setPage(1);
-        setRestaurants([]);
-        setCuisine(e.target.value);
-        setRenderedIds([]);
-    };
-
-    // handles the sort filter dropdown option changing
-    const handleSortChange = (e) => {
-        setPage(1);
-        setRestaurants([]);
-        setSortType(e.target.value);
-        setRenderedIds([]);
-    };
-
-    // fully resets both dropdowns and search parameters
-    const reset = (e) => {
-        e.preventDefault();
-        setCuisine("");
-        setSortType("best_match");
-        document.getElementById("cuisine-type-select").value = "";
-        document.getElementById("sort-type-select").value = "";
-    };
-
     // will load more restaurants once the user hits the bottom of the page
+    console.log(restaurants);
     useEffect(() => {
         const options = {
             root: null,
@@ -113,8 +37,7 @@ const RestaurantsList = () => {
                 observer.unobserve(loader.current);
             }
         };
-    }, []);
-
+    }, [loader]);
     // ensure the list of favourite restaurants for the user is up to date
     useEffect(() => {
         const getData = async () => {
@@ -142,40 +65,6 @@ const RestaurantsList = () => {
 
     return (
         <div className={`container`}>
-            <div className={styles.top}>
-                <div className={styles.options}>
-                    <select
-                        onChange={handleCuisineChange}
-                        id="cuisine-type-select"
-                        defaultValue="Select a Cuisine"
-                    >
-                        <option value="" selected disabled>
-                            Select a Cuisine
-                        </option>
-                        {cuisines.map((type) => {
-                            return (
-                                <option value={type.key}>{type.value}</option>
-                            );
-                        })}
-                    </select>
-                    <select onChange={handleSortChange} id="sort-type-select">
-                        <option value="" selected disabled>
-                            Filter By:
-                        </option>
-                        {filters.map((type) => {
-                            return (
-                                <option value={type.key}>{type.value}</option>
-                            );
-                        })}
-                    </select>
-                </div>
-                <div className={styles.buttonContainer}>
-                    <button className={styles.button} onClick={reset}>
-                        Reset
-                    </button>
-                </div>
-            </div>
-
             <div className={styles.restaurantsWrapper}>
                 {restaurants.map((restaurant, index) => (
                     <Restaurant
@@ -186,7 +75,19 @@ const RestaurantsList = () => {
                     />
                 ))}
             </div>
-            <div ref={loader} className={styles.loader}></div>
+
+            {noMoreRestaurants && (
+                <div>
+                    <p>
+                        There are no more restaurants that fit this criteria.
+                        Please change your criteria or location to find matching
+                        restaurants in your area.
+                    </p>
+                </div>
+            )}
+            {!noMoreRestaurants && (
+                <div ref={loader} className={styles.loader}></div>
+            )}
         </div>
     );
 };
--- frontend/lib/fetchRestaurantID.jsx
+++ frontend/lib/fetchRestaurantID.jsx
@@ -3,34 +3,21 @@ import axios from "axios";
 export default async function fetchRestaurantID(restaurantID, name) {
     let restaurant;
     let uuid;
-    console.log("fetchRestaurantID");
-    console.log(restaurantID);
-    console.log(name);
     if (restaurantID && name) {
         try {
             const response = await axios.get(
                 `${process.env.NEXT_PUBLIC_BASE_URL}/api/searchrestaurants?id=${restaurantID}`
             );
-            console.log(response);
             if (response.data.length > 0) {
                 restaurant = response.data[0];
                 uuid = restaurant.id;
-                console.log(
-                    "response from fetching restaurant id",
-                    response.data[0]
-                );
-                console.log("Restaurant UUID:", uuid);
                 return restaurant;
             } else {
-                console.log("Creating new restaurant.");
                 // need to add the restaurant to the collection
                 const createResponse = await axios.get(
                     `${process.env.NEXT_PUBLIC_BASE_URL}/api/addrestaurant?id=${restaurantID}&name=${name}`
                 );
                 restaurant = createResponse.data.data[0];
-                uuid = createResponse.data.data.id;
-                console.log("response", createResponse);
-                console.log("Created new restaurant with UUID:", uuid);
                 return restaurant;
             }
         } catch (error) {
