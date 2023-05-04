b1e2ae0f3848aa541b070cddaf6e9f828dea1f7c
FelixLiu666 <c0017299@newcastle.ac.uk>
2023-05-04 19:20:03 +0100

    Add more test case to test home page

--- frontend/cypress/e2e/click_register_button.cy.js
+++
@@ -1,9 +0,0 @@
-describe('My First Test', () => {
-  it('clicks the link "type"', () => {
-    cy.visit('http://localhost:3000')
-
-    cy.contains('Register').click()
-
-    cy.url().should('include', '/register')
-  })
-})
